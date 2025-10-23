44e15961ee5c584424a7e1ac962f7a27fe2ad330
mmcbbq <m.martinez@bbq.de>
2025-10-23 15:22:51 +0200

    api seed

--- migration/getall.php
+++ migration/getall.php
@@ -1,8 +1,6 @@
 <?php
 require_once '../src/database/datenbank.php';
-
 $curl = curl_init();
-
 curl_setopt_array($curl, array(
     CURLOPT_URL => 'https://pokeapi.co/api/v2/pokemon?limit=150',
     CURLOPT_RETURNTRANSFER => true,
@@ -13,9 +11,7 @@ curl_setopt_array($curl, array(
     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
     CURLOPT_CUSTOMREQUEST => 'GET',
 ));
-
 $response = curl_exec($curl);
-
 curl_close($curl);
 $poke_array = json_decode($response,true);
 $con = dbcon();
@@ -23,7 +19,7 @@ $sql = " INSERT INTO pokemon (name, caught, type) VALUES (:name, 0, :type)";
 $stmt = $con->prepare($sql);
 for ($i = 0; $i < count($poke_array["results"]); $i++) {
 
-    $name = $poke_array["results"][$i]['name'];
+    $poke_name = $poke_array["results"][$i]['name'];
     $poke_url = $poke_array["results"][$i]['url'];
     $poke_curl = curl_init();
     curl_setopt_array($poke_curl, array(
@@ -36,40 +32,15 @@ for ($i = 0; $i < count($poke_array["results"]); $i++) {
         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
         CURLOPT_CUSTOMREQUEST => 'GET',
     ));
-
     $pokemon_json = curl_exec($poke_curl);
     curl_close($poke_curl);
     $pokemon = json_decode($pokemon_json,true);
     $poketype = $pokemon['types'][0]['type']['name'];
-    $stmt->bindParam(":name",$name);
+    $stmt->bindParam(":name",$poke_name);
     $stmt->bindParam(":type",$poketype);
     $stmt->execute();
-
 }
 
-
-
-
-
-
-//$poke_curl = curl_init();
-//curl_setopt_array($poke_curl, array(
-//    CURLOPT_URL => $poke_url,
-//    CURLOPT_RETURNTRANSFER => true,
-//    CURLOPT_ENCODING => '',
-//    CURLOPT_MAXREDIRS => 10,
-//    CURLOPT_TIMEOUT => 0,
-//    CURLOPT_FOLLOWLOCATION => true,
-//    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-//    CURLOPT_CUSTOMREQUEST => 'GET',
-//));
-//
-//$pokemon_json = curl_exec($poke_curl);
-//$pokemon = json_decode($pokemon_json,true);
-
-//echo '<pre>';
-//var_dump($pokemon['types'][0]['type']['name']);
-//echo '</pre>';
 
 
 
